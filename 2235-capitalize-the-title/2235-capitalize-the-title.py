b7384fb25d1d2257eb647f5a441736c809ea6490
Murat <147882708+MuratBolatoglu@users.noreply.github.com>
2025-02-16 19:08:50 +0300

    Time: 33 ms (0.68%), Space: 14 MB (100%) - LeetHub

---
+++ 2235-capitalize-the-title/2235-capitalize-the-title.py
@@ -0,0 +1,7 @@
+class Solution:
+    def capitalizeTitle(self, title: str) -> str:
+        title=title.split(" ")
+        for i in range(0,len(title)):
+            if len(title[i])>2: title[i]=(title[i].lower()).capitalize()
+            else: title[i]=title[i].lower()
+        return " ".join(title)
