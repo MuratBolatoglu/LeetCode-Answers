43b42f1ace015c043a2fece514def44c217cbf6e
Murat <147882708+MuratBolatoglu@users.noreply.github.com>
2025-02-17 02:49:45 +0300

    Time: 4 ms (70.25%), Space: 23.9 MB (58.64%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& inter) {
+        if(inter.empty()) return {};
+        sort(inter.begin(),inter.end(),[](const vector<int> &i1, const vector<int> &i2){
+            return i1[0]<i2[0];
+        });
+        vector<vector<int>> list = { inter[0] };
+        for (size_t i = 1; i < inter.size(); i++) {
+            if (inter[i][0] <= list.back()[1]) {
+                list.back()[1] = max(inter[i][1], list.back()[1]);
+            }else list.push_back(inter[i]);
+        }
+        return list;    
+    }
+};
