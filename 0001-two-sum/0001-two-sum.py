14a850c054107325268bf5216f0ea22d9ca67496
Murat <147882708+MuratBolatoglu@users.noreply.github.com>
2025-02-16 19:15:06 +0300

    Time: 39 ms (50.03%), Space: 14.3 MB (16.16%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        prev={}
+        for i , n in enumerate(nums):
+            diff=target-n
+            if diff in prev:
+                return [prev[diff],i]
+            prev[n]=i
+        
