2a16efaecf4e3e587aaabe25898a2eb34e5fcbf5
Murat <147882708+MuratBolatoglu@users.noreply.github.com>
2025-02-19 17:50:45 +0300

    Time: 157 ms (5.05%), Space: 73.2 MB (5.05%) - LeetHub

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> ans;
+    string getHappyString(int n, int k) {
+        generate(n,"",0);
+        sort(ans.begin(),ans.end());
+        if(ans.size()<k) return "";
+        return ans[k-1];
+    }
+    void generate(int target,string current,int len){
+        vector<string> letters={"a","b","c"};
+        if(len==target){
+            ans.push_back(current);
+            return;
+        }
+        if(current==""){
+            for(auto& i : letters){
+                generate(target,i,len+1);
+            }
+        }else{
+            for(auto& i: letters){
+                if(i != string (1,current[len-1])) generate(target,current+i,len+1);
+            }
+        }
+    }
+};
