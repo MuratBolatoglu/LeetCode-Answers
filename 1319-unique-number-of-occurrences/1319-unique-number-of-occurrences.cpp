c4c9e73b12aab078d571ba4e489ff9e6f61158c4
Murat <147882708+MuratBolatoglu@users.noreply.github.com>
2025-03-06 02:28:14 +0300

    Time: 1 ms (26.14%), Space: 12.1 MB (27.43%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int> m;
+        unordered_set<int> s;
+        for(auto& i : arr) m[i]++;
+        for(auto& i: m)  if(s.find(i.second)!=s.end()) return false; else s.insert(i.second);   
+        return true;
+    }
+};
