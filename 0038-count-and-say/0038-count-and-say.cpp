2894eeb53ee27f95f6628f5c404a713bbb8e355d
Murat <147882708+MuratBolatoglu@users.noreply.github.com>
2025-04-03 04:19:30 +0300

    Time: 15 ms (15.74%), Space: 18.9 MB (14.69%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -3,26 +3,28 @@ public:
     string countAndSay(int n) {
         string start = "1";
         for (int j = 0; j < n - 1; j++) {
-            vector<pair<int, char>> c = counter(start);
+            vector<vector<string>> c = counter(start);
             string temp = "";
             for (auto& i : c) {
-                temp += to_string(i.first) + i.second;
+                temp += i[0] + i[1];  // Concatenate count and character
             }
             start = temp;
         }
         return start;
     }
-    vector<pair<int, char>> counter(string str) {
-        vector<pair<int, char>> ret;
+
+    vector<vector<string>> counter(string str) {
+        vector<vector<string>> ret;
         int curr = 1;
         for (size_t i = 1; i < str.size(); i++) {
-            if (str[i] == str[i - 1]) curr++;    
-            else {
-                ret.push_back({curr, str[i - 1]});
+            if (str[i] == str[i - 1]) {
+                curr++;
+            } else {
+                ret.push_back({to_string(curr), string(1, str[i - 1])}); // Store char as string
                 curr = 1;
             }
         }
-        ret.push_back({curr, str.back()});
+        ret.push_back({to_string(curr), string(1, str.back())});  // Add last counted group
         return ret;
     }
 };
