80f5da9a37cbca50345312c88e00fcb7111f7fd4
Murat <147882708+MuratBolatoglu@users.noreply.github.com>
2025-07-26 20:23:45 +0300

    Time: 0 ms (100%), Space: 7.8 MB (83.7%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==1) return true;
+        if(n%2 != 0) return false;
+        while(n>2){
+            if(n%2 != 0) return false;
+            n/=2;
+        } 
+        cout<<n;
+        return n==2;
+    }
+};
